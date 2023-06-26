6603f48c604a78c4e8e68a39274bf741bd438da8
NISHI CJ <nishijayaraj@gmail.com>
2023-06-26 17:18:41 +0530

    Added fragment to enclose tsx return statemment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,16 @@
+import { Fragment } from "react";
+
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item"> An Item</li>
-      <li className="list-group-item"> An Second Item</li>
-      <li className="list-group-item"> An Third Item</li>
-      <li className="list-group-item"> An Fourth Item</li>
-    </ul>
+    <Fragment>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item"> An Item</li>
+        <li className="list-group-item"> An Second Item</li>
+        <li className="list-group-item"> An Third Item</li>
+        <li className="list-group-item"> An Fourth Item</li>
+      </ul>
+    </Fragment>
   );
 }
 
