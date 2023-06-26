168590aa64d8a69227572d0710657974fcc4dd47
NISHI CJ <nishijayaraj@gmail.com>
2023-06-26 19:52:52 +0530

    Displayed list items dynamically using array map()

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,19 @@
 import {} from "react";
 
 function ListGroup() {
+  let items = ["US", "Tokyo", "Landon", "Paris", "Spain"];
+  <li className="list-group-item"> An Item</li>;
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item"> An Item</li>
-        <li className="list-group-item"> An Second Item</li>
-        <li className="list-group-item"> An Third Item</li>
-        <li className="list-group-item"> An Fourth Item</li>
+        {items.map((item) => {
+          return (
+            <li className="list-group-item" key={item}>
+              {item}
+            </li>
+          );
+        })}
       </ul>
     </>
   );
