6268efa088581b168e5829f55de78fbbd9b01225
NISHI CJ <nishijayaraj@gmail.com>
2023-06-26 16:30:47 +0530

    Added list component

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,12 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item"> An Item</li>
+      <li className="list-group-item"> An Second Item</li>
+      <li className="list-group-item"> An Third Item</li>
+      <li className="list-group-item"> An Fourth Item</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
